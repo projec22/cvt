8499364a46ee4b1a3037effd4e1384a6f8a7c2e7
projec22 <149667260+projec22@users.noreply.github.com>
2023-11-02 18:16:23 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const index = document.querySelector("main h2");
+
+    index.textContent = "Welcome to My CV";
+});
+alert("Welcome to BANAN RASHED ALFAHR CV");
